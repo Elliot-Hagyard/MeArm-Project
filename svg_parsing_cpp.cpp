4e0a08909bddceed21c51e2c33e60bb2710c00df
E23214 <elliotarlo@gmail.com>
2022-10-24 22:34:51 -0700

    Adding fstream output for testing

--- svg_parsing_cpp.cpp
+++ svg_parsing_cpp.cpp
@@ -2,28 +2,34 @@
 #include <fstream>
 #include <string.h>
 #include <iostream>
+#include <cmath>
+
 std::fstream file;
-const int buffer_len = 300;
+const int buffer_len = 400;
 char buffer[buffer_len];
 //path needs to retain info about the path command after parsing
-float path_array[8] = {0,0,0,0,0,0,0,0};
+float path_array[6] = {0,0,0,0,0,0};
 //Store the data from a line.
 int line_array[4] = {0,0,0,0};
 int circle_array[3] = {0,0,0};
 int rect_array[4] = {0,0,0,0};
+const short point_array_len = 25;
+float point_array[point_array_len][2];
 //Just a char array
-char* draw_types[6];
 //Need to retain this info for lower case commands in svg
-float cur_pos[2]; 
+float cur_pos[2];
 float initial_points[2];
 char begining_of_string[10];
+std::ofstream myFile;
 
-char path[5]= "path";
-char polyline[9] = "polyline";
-char rect[5] = "rect";
-char line[5] = "line";
-char polygon[8] = "polygon";
-char circle[7] = "circle";
+static const short x = 0;
+const short y = 1;
+const char path[5]= "path";
+const char polyline[9] = "polyline";
+const char rect[5] = "rect";
+const char line[5] = "line";
+const char polygon[8] = "polygon";
+const char circle[7] = "circle";
 
 void line_command(){
   //TODO: Read from the global line_array and then make the mearm actually draw it
@@ -31,26 +37,100 @@ void line_command(){
   std::cout<<"y1: "<<line_array[1]<<std::endl;
   std::cout<<"x2: " << line_array[2]<<std::endl;
   std::cout<<"y2: "<<line_array[3]<<std::endl;
+  myFile<<"L,"<<line_array[0]<<","<<line_array[1]<<"\n";
+  myFile<<"L,"<<line_array[2]<<","<<line_array[3]<<"\n";
+  myFile<<"L,"<<line_array[0]<<","<<line_array[1]<<"\n";
+  myFile<<"L,"<<line_array[2]<<","<<line_array[3]<<"\n";
+  myFile<<"e\n";
+  cur_pos[1] = line_array[3];
+  cur_pos[0] = line_array[2];
 
 }
 void circle_command(){
     std::cout<<"cx: "<< circle_array[0]<<std::endl;
     std::cout<<"cy: "<< circle_array[1]<<std::endl;
     std::cout<<"r: "<< circle_array[2]<<std::endl;
+    int index = 0;
+    float i = 0;
+    while(i < 3.141516*2){
+        point_array[index][x] = std::cos(i)*circle_array[2]+circle_array[0];
+        point_array[index][y] = std::sin(i)*circle_array[2]+circle_array[1];
+        myFile<<"c,"<<point_array[index][0]<<","<<point_array[index][1]<<"\n";
+        i+=1.0/point_array_len;
+        index+=1;
+        i+= (3.1415*2) / point_array_len;
+    }
+    myFile<<"c,"<<point_array[0][0]<<","<<point_array[0][1]<<"\n";
+    myFile<<"e\n";
+    cur_pos[0] = point_array[index][0];
+    cur_pos[1] = point_array[index][1];
 
 }
 void rect_command(){
-    std::cout<<"x: "<<rect_array[0]<<std::endl;
-    std::cout<<"y: "<<rect_array[1]<<std::endl;
-    std::cout<<"w: "<<rect_array[2]<<std::endl;
-    std::cout<<"h: "<<rect_array[3]<<std::endl;
+    const short x = rect_array[0];
+    const short y = rect_array[1];
+    const short w = rect_array[2];
+    const short h = rect_array[3];
+    //Line one (x-w/2, y-h/2) -> (x-w/2,y+h/2)
+    //Line Two (cur_pos[x], cur_pos[y], cur_pos[x]+w, cur_pos[y])
+    //line three (cur_pos[x],cur_pos[y], cur_pos[x], cur_pos[y]-h)
+    //line 4 (cur_pos[x], cur_pos[y], cur_pos[x]-w, cur_pos[y]);
+    line_array[0] = x;
+    line_array[1] = y;
+    line_array[2] = x;
+    line_array[3] = y + h;
+    line_command();
+    line_array[0] = cur_pos[0];
+    line_array[1] = cur_pos[1];
+    line_array[2] = cur_pos[0]+w;
+    line_array[3] = cur_pos[1];
+    line_command();
+    line_array[0] = cur_pos[0];
+    line_array[1] = cur_pos[1];
+    line_array[2] = cur_pos[0];
+    line_array[3] = cur_pos[1]-h;
+    line_command();
+    line_array[0] = cur_pos[0];
+    line_array[1] = cur_pos[1];
+    line_array[2] = cur_pos[0]-w;
+    line_array[3] = cur_pos[1];
+    line_command();
+
+}
+int cubicBezierCurve(float cX1, float cY1, float cX2, float cY2, float finalX, float finalY){
+
+
+    std::cout<<"Here";
+    float i = 0;
+    int index = 0;
+    while(i < 1){
+        point_array[index][x] = (1-i)*(1-i)*(1-i)*cur_pos[x] + 3*(1-i)*(1-i)*i*cX1 + 3*(1-i)*i*i*cX2 + i*i*i*finalX;
+        point_array[index][y] = (1-i)*(1-i)*(1-i)*cur_pos[y] + 3*(1-i)*(1-i)*i*cY1 + 3*(1-i)*i*i*cY2 + i*i*i*finalY;
+        i+=1.0/point_array_len;
+        myFile<<"c,"<<point_array[index][x]<<","<<point_array[index][y]<<"\n";
+        i+=1.0/point_array_len;
+        index+=1;
+    }
+    myFile<<"e\n";
+    cur_pos[0] = point_array[index-1][0];
+    cur_pos[1] = point_array[index-1][1];
+}
+int quadraticBezierCurve(float cX1, float cY1, float finalX, float finalY){
+    float i = 0;
+    int index = 0;
+    while(i < 1){
+        point_array[index][x] = (1-i)*(1-i)*cur_pos[x] + 2*(1-i)*i*cX1 + i*i*finalX;
+        point_array[index][y] = (1-i)*(1-i)*cur_pos[y] + 2*(1-i)*i*cY1 + i*i*finalY;
+        myFile<<"c,"<<point_array[index][x]<<","<<point_array[index][y]<<"\n";
+        i+=1.0/point_array_len;
+        index+=1;
+    }
 
 }
 void path_command(char command){
-  //TODO Implement specfic behavior for each path type
-  if(int(command) < int('A')){
+  if(int(command) >= int('a')){
       //translate the relative commands to be absolute
-      for(int i =0; i < 8; i++)
+      for(int i =0; i < 6; i++)
         path_array[i]+=cur_pos[i%2];
   }
   switch(command){
@@ -60,12 +140,15 @@ void path_command(char command){
       //Move abs
       //M moves the baseline point
       //lift_pen(); //placeholder function
+      //Path array contains 2 points
       //Move x and y. NOTE X AND Y in the svg are X and Z in our
       std::cout<<"Move"<<std::endl;
       std::cout<<"x"<<path_array[0]<<'\t'<<"y"<<path_array[1]<<std::endl;
       //go_to(x,y);
-      //initial_point[0]=path_array[0]
-      //initial_point[1]=path_array[1]
+      cur_pos[x] = path_array[x];
+      cur_pos[y] = path_array[y];
+      initial_points[x]=path_array[x];
+      initial_points[y]=path_array[y];
       break;
     }
     case 's':{}
@@ -77,31 +160,61 @@ void path_command(char command){
     case 'c':{}
     case 'C':{
         std::cout<<"cubic curve"<<std::endl;
-      //Is anyhting else needed here?
-      //cubic_bezier_curve();
+      //Path array should contain 6 points
+      //Also uses the gloabal var of the cur_pos
+        cubicBezierCurve(
+                path_array[0],
+                path_array[1],
+                path_array[2],
+                path_array[3],
+                path_array[4],
+                path_array[5]);
       break;
       }
     case 'q':
     case 'Q':
+        //Path array should contain 4 points
+        //Also uses the gloabal var of the cur_pos
         std::cout<<"Quadratic Curve"<<std::endl;
+          quadraticBezierCurve(
+                  path_array[0],
+                  path_array[1],
+                  path_array[2],
+                  path_array[3]);
       //quadratic bezier curve
       break;
     case 'z':{}
     case 'Z':{
       //Close path command. I think it just draws a straight line to the iniital point
-      //goto(inital_points[0],initial_points[1])
+      //Path array should contain 2 points
+      line_array[x] = cur_pos[x];
+      line_array[y] = cur_pos[y];
+      line_array[x+2] = initial_points[x];
+      line_array[y+2] = initial_points[y];
+      line_command();
       std::cout<<"Go to inital x and y"<<std::endl;
       break;
     }
     case 'h':{}
     case 'H':{
-      //Just a horiziontal line
+      //Just a horiziontal line should contain one point in path array
+        line_array[0] = cur_pos[x]; //
+        line_array[1] = cur_pos[y];
+        line_array[2] = path_array[0];
+        line_array[3] = cur_pos[y];
+        line_command();
         std::cout<<"Move across to:"<<path_array[0]<<std::endl;
       //move_to(path_ar[0], cur_y)
       break;
     }
     case 'l':{}
     case 'L':{
+        //contains 2 points in path array
+        line_array[x] = cur_pos[x]; //x
+        line_array[y] = cur_pos[y]; // y
+        line_array[x+2] = path_array[x]; //x_final
+        line_array[y+2] = path_array[y]; //y final
+        line_command();
         std::cout<<"Move x:"<<path_array[0]<<std::endl;
         std::cout<<"Move y:"<<path_array[1]<<std::endl;
       //Line
@@ -109,6 +222,12 @@ void path_command(char command){
       }
     case 'v':{}
     case 'V':{
+        //path array should have one point
+        line_array[0] = cur_pos[x]; //x
+        line_array[1] = cur_pos[y]; // y
+        line_array[2] = cur_pos[x]; //x_final
+        line_array[3] = path_array[0]; //y final
+        line_command();
         std::cout<<"Move up to:"<<path_array[0]<<std::endl;
         //move_to(cur_x, path_ar[0])
       //Vertiacal line
@@ -133,13 +252,14 @@ int parse_path_command(int idx){
       while(int(buffer[idx]) < int('A') and buffer[idx] != '"') {
           if (buffer[idx] == '.') {
               idx += 1;
-              while (int(buffer[idx]) < int('A') and int(buffer[idx]) > '0') {
+              while (int(buffer[idx]) <= int('9') and int(buffer[idx]) >= '0') {
                   idx += 1;
               }
 
           }
           else {
               if (buffer[idx] < '0') {
+                  neg = false;
                   //Ascii values for - and , are less than ascii value for '0'
                   if (buffer[idx] == '-') {
                       neg = true;
@@ -182,7 +302,12 @@ int get_num_from_quote(int num_array[], int index_in_array, int index_in_buffer)
     //THis logic should probably be in a function, but will need to revisit that
     while(buffer[index_in_buffer]!='"'){
         if(buffer[index_in_buffer]=='.'){
+            index_in_buffer+=1;
+            if( buffer[index_in_buffer] > '5'){
+                num_array[index_in_array]+=1;
+            }
             while(buffer[index_in_buffer]!='"'){
+
                 //Choping off the decimal for now
                 index_in_buffer+=1;
             }
@@ -226,6 +351,8 @@ void parse_line(int size_of_line){
   for (int i = 0; i<10; i++){
     std::cout<<begining_of_string[i];
   }
+  cur_pos[0] = 0;
+  cur_pos[1] = 0;
   std::cout<<std::endl;
   if(strstr(begining_of_string,path)){
     //Should parse <path fill-rule="evenodd" fill="none" stroke="#000000" stroke-miterlimit="10" d="M73.5,31.5c0.3,1.9,0.5,6-2,9
@@ -390,6 +517,10 @@ int readLine()
     for(int i=0; i <4; i++)
         line_array[i]=0;
     int idx = 0;
+    while(file.peek() == '\t' or file.peek() == '\n' or file.peek() == ' '){
+        file.get();
+    }
+
     while(true){
         //Return wehn you encounter a newline
         if (file.peek() != '>' and file.peek() != EOF){
@@ -412,7 +543,9 @@ int readLine()
 }
 
 int main(void){
-    file.open("/Users/elliothagyard/Documents/ISTA303/glasses.svg",std::fstream::in);
+    myFile.open("./example.csv");
+    myFile<<"X,Y\n";
+    file.open("/Users/elliothagyard/Documents/Untitled-1.svg",std::fstream::in);
     while( not file.eof()){
     int len_of_line = readLine();
     std::cout<<len_of_line;
@@ -422,5 +555,6 @@ int main(void){
     std::cout<<std::endl;
     parse_line(len_of_line);
     }
+    myFile.close();
     file.close();
 }
